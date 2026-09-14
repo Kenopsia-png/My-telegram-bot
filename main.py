35d21aee90212a14976b68ed92f7c70c26712737
Kenopsia-png <susssies666@gmail.com>
2026-09-14 11:59:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ dp = Dispatcher()
 
 def ask_gemini(prompt_text):
     """Запрос к Gemini API с правильной авторизацией через заголовки"""
-    url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent"
+    url = "f"https://generativelanguage.googleapis.com/v1beta/models/gemini-3.6-flash:generateContent?key={GEMINI_API_KEY}"
     
     # Передаем ключ авторизации через спец-заголовок x-goog-api-key
     headers = {
